8cc9a9d44ecc0cd1e8560f03702582dd73610e1f
vikinatora <vikinatora@gmail.com>
2022-06-29 09:29:07 +0300

    add logging to health check

--- src/modules/Infra/eth-health/eth-health.service.ts
+++ src/modules/Infra/eth-health/eth-health.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import {
   HealthIndicator,
   HealthIndicatorResult,
@@ -9,6 +9,8 @@ import R from 'ramda';
 
 @Injectable()
 export class EthHealthIndicator extends HealthIndicator {
+  private readonly logger = new Logger(EthereumService.name);
+
   constructor(private ethService: EthereumService) {
     super();
   }
@@ -21,9 +23,11 @@ export class EthHealthIndicator extends HealthIndicator {
     const result = this.getStatus(key, isHealthy, { blockNumber, network });
 
     if (isHealthy) {
+      this.logger.log('Health check succeeded.');
       return result;
     }
 
+    this.logger.error('Health check failed.');
     throw new HealthCheckError(
       'infura health check failed',
       'block number is null or undefined',
